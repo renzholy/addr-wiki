8e1fcc763096b0ae245bcdbb3be2b57198881a90
Found Pan Tiger <renzholy@gmail.com>
2022-06-05 20:01:42 +0800

    update icons

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/icons/.*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=43200, immutable",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
