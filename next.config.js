8e6b30f21f28aa8745626647b5f1980bd9743196
Found Pan Tiger <renzholy@gmail.com>
2022-06-08 16:07:30 +0800

    update

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
         headers: [
           {
             key: "cache-control",
-            value: "public, max-age=43200, immutable",
+            value: "public, max-age=2592000, immutable",
           },
         ],
       },
