7efbd78972bdf0236943fc97a37ae08f7a55e28b
Found Pan Tiger <renzholy@gmail.com>
2022-10-12 17:52:03 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,23 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
+import Script from "next/script";
 import "../styles/global.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-0F5B2L3F33"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-0F5B2L3F33');
+        `}
+      </Script>
       <Head>
         <title>ADDR•WIKI</title>
         <base target="_blank" />
